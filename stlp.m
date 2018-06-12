e32a45869727a68b88a85015694266117168b276
vitocormun <cormun@bc.edu>
2018-06-12 18:58:33 -0400

    introduce pc and lags of shocks in the 1st stage

--- stlp.m
+++ stlp.m
@@ -2,6 +2,8 @@ function [IRF_E, IRF_R, IRF_L ] = stlp(y,x,u,fz,lags,H)
 % H is the length of the irf
 % y and x are Tx1;
 % the regression is  y(t+h) = A0 + PI^E(1-F(z(t-1)))u(t) + PI^RF(z(t-1))u(t) + B1u(t-1) + C1y(t-1) + D1x(t-1) + C2y(t-2) + ...
+
+    FZ = repmat(fz,1,size(x,2));
 for h = 1:H
     Y = y(h+lags:end,:);
     X = u(lags+1:end-h+1,:); %make sure fz is lagged in the argument of the function
@@ -10,8 +12,9 @@ for h = 1:H
     if lags > 0 %which allows for controls
         for jj = 1:lags;
             if x(1)^2 > 0; 
-            X  = [X,u(lags-jj+1:end-jj-h+1,:), u(lags-jj+1:end-jj-h+1,:).*fz(lags+1:end-h+1,1),y(lags-jj+1:end-jj-h+1,:), y(lags-jj+1:end-jj-h+1,:).*fz(lags+1:end-h+1,1),...
-               x(lags-jj+1:end-jj-h+1,:),x(lags-jj+1:end-jj-h+1,:).*fz(lags+1:end-h+1,1)];
+            X  = [X,u(lags-jj+1:end-jj-h+1,:), u(lags-jj+1:end-jj-h+1,:).*fz(lags+1:end-h+1,1), ...
+               y(lags-jj+1:end-jj-h+1,:), y(lags-jj+1:end-jj-h+1,:).*fz(lags+1:end-h+1,1),...
+               x(lags-jj+1:end-jj-h+1,:),x(lags-jj+1:end-jj-h+1,:).*FZ(lags+1:end-h+1,:)];
             XL = [XL, u(lags-jj+1:end-jj-h+1,:), y(lags-jj+1:end-jj-h+1,:),...
                x(lags-jj+1:end-jj-h+1,:)];
             else 
