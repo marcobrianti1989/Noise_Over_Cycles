6522fa8a675b9fce1ccf5be0e4ef806ba27171e6
Marco Brianti <marcobrianti@live.it>
2018-06-12 13:30:11 -0400

    PC adjusted

--- read_PC.m
+++ read_PC.m
@@ -4,8 +4,13 @@ close all
 filename = 'Dataset_test_PC';
 sheet = 'Quarterly';
 range = 'B2:DA300';
-do_truncation = 1; %Do truncate data. 
+do_truncation = 1; %Do truncate data.
 [dataset, var_names] = read_data2(filename, sheet, range, do_truncation);
 dataset = real(dataset);
 
 pc = get_principal_components(dataset);
+
+hold on
+for i=1:5
+      plot(pc(:,i))
+end
