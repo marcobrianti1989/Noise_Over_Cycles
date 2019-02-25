e54fd31767e9a465757d072cd1b83e50eb632f02
Marco Brianti <marcobrianti@live.it>
2019-02-25 18:47:16 -0500

    IVSVAR

---
+++ lin_timetrend.m
@@ -0,0 +1,21 @@
+function detrended_data = lin_timetrend(data,q)
+
+% Simple OLS with Time Trend and Constant
+T              = length(data);
+Y              = data;
+trend          = linspace(1,T,T)';
+trendq         = trend.^2;
+if q == 0
+      X              = [ones(T,1) trend];
+      disp('Variables are linearly detrended')
+      fprintf('\n')
+else
+      X              = [ones(T,1) trend trendq];
+      disp('Variables are quadratically detrended')
+      fprintf('\n')
+end
+B              = (X'*X)^(-1)*X'*Y;
+detrended_data = Y - X*B;
+
+
+end
--- tech_info_table.m
+++ tech_info_table.m
@@ -5,31 +5,41 @@ disp('           Technical Parameters LP              ')
 disp('------------------------------------------------')
 fprintf('\n')
 if strcmp(which_Z,'1') == 1
-      disp('Survey                  SPF Real GDP Growth')
+      disp('Survey                     SPF Real GDP Growth')
 elseif strcmp(which_Z,'2') == 1
-      disp('Survey                  SPF Nominal GDP Growth')
+      disp('Survey                     SPF Nominal GDP Growth')
 elseif strcmp(which_Z,'3') == 1
-      disp('Survey                  SPF Real Consumption Growth')
+      disp('Survey                     SPF Real Consumption Growth')
 elseif strcmp(which_Z,'4') == 1
-      disp('Survey                  SPF Industrial Production Growth')
+      disp('Survey                     SPF Industrial Production Growth')
 elseif strcmp(which_Z,'5') == 1
-      disp('Survey                  SPF Real Total Investment Growth')
+      disp('Survey                     SPF Real Total Investment Growth')
 elseif strcmp(which_Z,'6') == 1
-      disp('Survey                  SPF Consumer Price Index')
+      disp('Survey                     SPF Consumer Price Index')
 elseif strcmp(which_Z,'7') == 1
-      disp('Survey                  Michigan Index Confidence')
+      disp('Survey                     Michigan Index Confidence')
 end
-disp(['Number of lags          ',num2str(lags)])
-disp(['Number of leads         ',num2str(leads)])
-disp(['Number of lags in LP    ',num2str(lags_LP)])
-disp(['Trend                   ',which_trend])
-disp(['IRFs horizon            ',num2str(H)])
-disp(['Number of PCs           ',num2str(nPC)])
-disp(['Number of simulations   ',num2str(nsimul)])
+disp(['Number of lags 1st Step    ',num2str(lags)])
+disp(['Number of leads            ',num2str(leads)])
+disp(['Number of lags in LP       ',num2str(lags_LP)])
+disp(['Trend                      ',which_trend])
+disp(['IRFs horizon               ',num2str(H)])
+disp(['Number of PCs 1st Step     ',num2str(nPC_first)])
+disp(['Number of PCs in LP        ',num2str(nPC_LP)])
+disp(['Number of simulations      ',num2str(nsimul)])
 if diff_LP == 1
-      disp('Other                   Dependent Variables are differentiated')
+      disp('Other                      Dependent Variables are differentiated')
 else
-      disp('Other                   Dependent Variables are in levels')
+      disp('Other                      Dependent Variables are in levels')
 end
 fprintf('\n')
+disp('------------------------------------------------')
 fprintf('\n')
+for ishock = 1:length(which_shock)
+      disp([which_shock{ishock}])
+      fprintf('\n')
+      for ii = 1:length(varlist)
+            disp(['  - Degree of freedom for ',varlist{ii},' are on average: ',num2str(floor(mean(DFkk(ii,:,ishock))))])
+      end
+      fprintf('\n')
+end
